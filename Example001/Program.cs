acca6f713d9ced6917885e8687fae2b85fe439a6
Dmitry <sicily007@rambler.ru>
2023-08-12 18:27:20 +0300

    add comments

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -13,12 +13,13 @@ string userData = System.Console.ReadLine();
 string zapyataya = ",";
 string tochka = ".";
 int indexOfZap = userData.IndexOf(zapyataya); /* присваивает целочисленной indexOfZap значение индекса переменной zapyataya
-                                                иными словами ищем в строке запятую "," */
+                                                иными словами ищем в строке запятую "," и запоминаем ее индекс */
 int indexOfToch = userData.IndexOf(tochka);
 
-if (indexOfZap == 1 | indexOfToch == 1)
+if (indexOfZap == 1 | indexOfToch == 1) /* определям размер числа, т.е. 0,11111 или 12131,0054, т.е., у числа менее 10, ","
+                                        всегда будет иметь значение 1 */
 {
-    if (indexOfZap > 0)
+    if (indexOfZap > 0) // если indexOfZap > 0, то перед нами число менее 10
     {
         string[] strokiZap = userData.Split(new char [] {','}); /* разбиваем строку userData по символу ',', затем присваиваем массиву strokiZap
                                                                 значение двух получившихся массивов */
@@ -30,7 +31,9 @@ if (indexOfZap == 1 | indexOfToch == 1)
         System.Console.WriteLine(strokiTochka[0] + "." + strokiTochka[1].Substring(1));
     }
 } 
-else
+else /* в противном случае, у нас число более 10    
+        разбиваю строку на 2 части: firstPart - часть строки userData с индекса 0 длиной 1 элемент
+                                    secondPart - часть строки userData с индекса 2 длиной до конца строки */
 {
     string firstPart = userData.Substring(0,1);
     string secondPart = userData.Substring(2);
